cb2ad853dadc54911cff9c8cbd92800c14f9671a
Gabriel <gabrieloctavioperez@gmail.com>
2021-12-30 13:48:45 -0400

    Navegacion

--- js/app.js
+++ js/app.js
@@ -1,17 +1,29 @@
 (function () {
-	var prevScrollpos = window.pageYOffset;
-	var navbar = document.getElementById('navbar');
-	if (prevScrollpos == true) {
-		navbar.classList.add('bg-white');
-	}
-	window.onscroll = function () {
-		var currentScrollPos = window.pageYOffset;
+	hideFunction();
 
-		if (prevScrollpos > currentScrollPos) {
-			navbar.style.top = '0';
-		} else {
-			navbar.style.top = '-90px';
-		}
-		prevScrollpos = currentScrollPos;
-	};
+	function hideFunction() {
+		var prevScrollpos = window.pageYOffset;
+		var navbar = document.getElementById('navbar');
+		var navigation = document.getElementById('navigation');
+
+		window.onscroll = function () {
+			if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+				navbar.classList.remove('py-4');
+
+				navigation.classList.add('bg-dark');
+			} else {
+				navbar.classList.add('py-4');
+
+				navigation.classList.remove('bg-dark');
+			}
+
+			var currentScrollPos = window.pageYOffset;
+			if (prevScrollpos > currentScrollPos) {
+				navigation.style.top = '0';
+			} else {
+				navigation.style.top = '-60px';
+			}
+			prevScrollpos = currentScrollPos;
+		};
+	}
 })();
